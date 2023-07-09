5f9c0c8f3365e5823e57fe8512551dc7e127ce23
DESKTOP-V9V870H\MAHOR <mahorjitendra@gmail.com>
2023-07-08 20:27:45 -0700

    Azure Blob Storage Changes

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,4 @@
-FROM maven:3.8.1-openjdk-17-slim AS build
-COPY src /usr/src/app/src
-COPY pom.xml /usr/src/app
-RUN mvn clean package -DskipTests
-
-#
-# PACKAGE STAGE
-#
-FROM openjdk:17.0.1-jdk-slim
-COPY --from=build /usr/src/app/target/Azure-Service-bus-0.0.1-SNAPSHOT.jar /usr/app/Azure-Service-bus-0.0.1-SNAPSHOT.jar
+FROM openjdk
 EXPOSE 8080
-CMD ["java","-jar","/usr/app/Azure-Service-bus-0.0.1-SNAPSHOT.jar"]
+ADD target/Azure-Service-bus-0.0.1-SNAPSHOT.jar Azure-Service-bus-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java" ,"-jar","/Azure-Service-bus-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/coderjeet/microservicerabbitMQ/EventController.java
+++ src/main/java/com/coderjeet/microservicerabbitMQ/EventController.java
@@ -1,15 +1,21 @@
 package com.coderjeet.microservicerabbitMQ;
 
+
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.coderjeet.microservicerabbitMQ.sender.AzbQueueSender;
 import com.coderjeet.microservicerabbitMQ.sender.AzbTopicPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 @RestController
 @RequestMapping("/v1/service/")
@@ -36,5 +42,23 @@ public class EventController {
         topicPublisher.send(msg);
         String subscriptionId =  secretClient.getSecret("subscriptionId").getValue();
         LOGGER.info("ConnectionString Fetched from key Vault {}",subscriptionId);
+    }
+
+    @Value("azure-blob://testcontainer/test.txt")
+    private Resource blobFile;
+
+    @GetMapping("/readBlobFile")
+    public String readBlobFile() throws IOException {
+        return StreamUtils.copyToString(
+                this.blobFile.getInputStream(),
+                Charset.defaultCharset());
+    }
+
+    @PostMapping("/writeBlobFile")
+    public String writeBlobFile(@RequestBody String data) throws IOException {
+        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
+            os.write(data.getBytes());
+        }
+        return "file was updated";
     }
 }
