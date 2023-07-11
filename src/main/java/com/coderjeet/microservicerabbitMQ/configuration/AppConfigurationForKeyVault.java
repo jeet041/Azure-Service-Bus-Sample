d109ee96046b9dcd0e1e4a977c293fd995c66880
jeet041 <mahorjitendra@gmail.com>
2023-07-11 17:40:04 +0530

    Added Blob Storage code

--- src/main/java/com/coderjeet/microservicerabbitMQ/configuration/AppConfigurationForKeyVault.java
+++ src/main/java/com/coderjeet/microservicerabbitMQ/configuration/AppConfigurationForKeyVault.java
@@ -1,7 +1,6 @@
 package com.coderjeet.microservicerabbitMQ.configuration;
 
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.*;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import org.springframework.amqp.core.Binding;
@@ -18,17 +17,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfigurationForKeyVault {
 
+	@Value("${app.config.azure.client-secret}")
+	private String clientSecret;
+	@Value("${app.config.azure.client-id}")
+	private String clientId;
+	@Value("${app.config.azure.tenant-id}")
+	private String tenantId;
+
 	@Value("${spring.cloud.azure.keyvault.secret.property-sources[0].endpoint}")
 	private String vaultUri;
 	/**
 	 * The default credential first checks environment variables for configuration.
 	 * If environment configuration is incomplete, it will try managed identity.
 	 */
+
 		@Bean
 		public SecretClient createSecretClient() {
+			ClientSecretCredential clientSecretCredentialBuilder= new ClientSecretCredentialBuilder().
+					clientSecret(clientSecret)
+					.clientId(clientId)
+					.tenantId(tenantId)
+					.build();
 			return new SecretClientBuilder()
 					.vaultUrl(vaultUri)
-					.credential(new DefaultAzureCredentialBuilder().build())
+					.credential(clientSecretCredentialBuilder)
 					.buildClient();
 		}
+
+//	@Bean
+//	public SecretClient createSecretClient() {
+//		return new SecretClientBuilder()
+//				.vaultUrl(vaultUri)
+//				.credential(new ManagedIdentityCredentialBuilder().clientId("9ff186f9-d19d-4730-91a4-d69d6fce690d").build())
+//				.buildClient();
+//	}
 }
---
+++ src/main/java/com/coderjeet/microservicerabbitMQ/controller/BlobController.java
@@ -0,0 +1,73 @@
+package com.coderjeet.microservicerabbitMQ.controller;
+
+
+import com.azure.spring.cloud.core.resource.AzureStorageBlobProtocolResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/blob")
+public class BlobController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(BlobController.class);
+    private final String containerName;
+    private final ResourceLoader resourceLoader;
+    private final AzureStorageBlobProtocolResolver azureStorageBlobProtocolResolver;
+    static final String BLOB_RESOURCE_PATTERN = "azure-blob://%s/%s";
+
+    @Value("${spring.cloud.azure.storage.blob.account-name}")
+    private String accountName;
+    @Value("${spring.cloud.azure.storage.blob.endpoint}")
+    private String endpoint;
+
+    public BlobController(@Value("${spring.cloud.azure.storage.blob.container-name}") String containerName,
+                          ResourceLoader resourceLoader,
+                          AzureStorageBlobProtocolResolver patternResolver) {
+        this.containerName = containerName;
+        this.resourceLoader = resourceLoader;
+        this.azureStorageBlobProtocolResolver = patternResolver;
+    }
+
+    /**
+     * Using AzureStorageBlobProtocolResolver to get Azure Storage Blob resources with file pattern.
+     *
+     * @return fileNames in the container match pattern: *.txt
+     */
+    @GetMapping("/getFiles")
+    public List<String> listTxtFiles() throws IOException {
+        Resource[] resources = azureStorageBlobProtocolResolver.getResources(String.format(BLOB_RESOURCE_PATTERN, this.containerName, "*.txt"));
+        logger.info("{} resources founded with pattern:*.txt",resources.length);
+        return Stream.of(resources).map(Resource::getFilename).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{fileName}")
+    public String readResource(@PathVariable("fileName") String fileName) throws IOException {
+        logger.info("Account name:{}",accountName);
+        Resource resource = resourceLoader.getResource(String.format(BLOB_RESOURCE_PATTERN, this.containerName, fileName));
+        return StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+    }
+
+    @PostMapping("/{fileName}")
+    public String writeResource(@PathVariable("fileName") String fileName, @RequestBody String data) throws IOException {
+        logger.info("ENDPOINT name:{}",endpoint);
+        Resource resource = resourceLoader.getResource(String.format(BLOB_RESOURCE_PATTERN, this.containerName, fileName));
+        try (OutputStream os = ((WritableResource) resource).getOutputStream()) {
+            os.write(data.getBytes());
+        }
+        return "blob was updated";
+    }
+}
